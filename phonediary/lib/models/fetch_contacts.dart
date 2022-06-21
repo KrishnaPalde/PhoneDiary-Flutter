be3ff85b79287d3f81697aa7c023c1953b9592a1
KrishnaPalde <paldekrishna4325@gmail.com>
2022-06-21 12:52:45 +0530

    Completed

--- phonediary/lib/models/fetch_contacts.dart
+++ phonediary/lib/models/fetch_contacts.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:phonediary/models/single_contact.dart';
