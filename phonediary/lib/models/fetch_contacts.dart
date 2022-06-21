3afab902f9303098e25e8858ee2a235de3834f07
KrishnaPalde <paldekrishna4325@gmail.com>
2022-06-21 13:09:24 +0530

    Completed

--- phonediary/lib/models/fetch_contacts.dart
+++ phonediary/lib/models/fetch_contacts.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:phonediary/models/single_contact.dart';
@@ -10,9 +11,16 @@ class FetchContacts extends ChangeNotifier {
     return [..._contacts];
   }
 
+  String? _getCurrentUserEmail()
+  {
+    var cue = FirebaseAuth.instance.currentUser?.email;
+    cue = cue?.substring(0,cue.indexOf("@"));
+    return cue;
+  }
   void uploadAndAddContact(SingleContact con){
+    
     // Upload to firebase database
-    FirebaseDatabase.instance.ref("contacts").child(con.contactName).set(con.toMap());
+    FirebaseDatabase.instance.ref(_getCurrentUserEmail()! + "/contacts").child(con.contactName).set(con.toMap());
     fetchAllContacts();
   }
 
@@ -21,21 +29,21 @@ class FetchContacts extends ChangeNotifier {
     // Update Contact on firebase
     // FirebaseDatabase.instance.ref("contacts").child(oldCon.contactName).set(newCon.toMap());
 
-    FirebaseDatabase.instance.ref("contacts").child(oldCon.contactName).remove();
-    FirebaseDatabase.instance.ref("contacts").child(newCon.contactName).set(newCon.toMap());
+    FirebaseDatabase.instance.ref(_getCurrentUserEmail()! + "/contacts").child(oldCon.contactName).remove();
+    FirebaseDatabase.instance.ref(_getCurrentUserEmail()! + "/contacts").child(newCon.contactName).set(newCon.toMap());
     // Then call fetchAllContacts
     fetchAllContacts();
   }
   void deleteContact(SingleContact con){
     // Delete from firebase database
-    FirebaseDatabase.instance.ref("contacts").child(con.contactName).remove();
+    FirebaseDatabase.instance.ref(_getCurrentUserEmail()! + "/contacts").child(con.contactName).remove();
     fetchAllContacts();
     notifyListeners();
   }
 
  Future<void> fetchAllContacts() async {
     List<SingleContact> _con = [];
-    var snapshot = await FirebaseDatabase.instance.ref("contacts").get();
+    var snapshot = await FirebaseDatabase.instance.ref(_getCurrentUserEmail()! + "/contacts").get();
     var data = snapshot.children;
     print(data.elementAt(0).value);
     for (var i = 0; i < data.length; i++) {
